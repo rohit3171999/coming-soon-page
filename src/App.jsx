f507fd2f894564b2d7204f2de9cc00878267b607
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 15:37:36 +0530

    add markup in SocialLinks.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/HeroSection.jsx";
 import SignUpForm from "./components/SignUpForm.jsx";
+import SocialLinks from "./components/SocialLinks.jsx";
 
 function App(){
     return (
         <>
             <HeroSection/>
             <SignUpForm/>
+            <SocialLinks/>
         </>
 
     );
