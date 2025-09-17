8e06ba3b21d6802f1a5a2e2f3546e0b06e9bf38a
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 15:27:10 +0530

    add functional component in CountdownWriter.jsx

---
+++ src/components/CountdownWriter.jsx
@@ -0,0 +1,21 @@
+/*
+* Description: Displays the dynamic countdown to launch date/time.
+Purpose: Build anticipation by showing exact time left until launch.
+* */
+export default function CountdownWriter(){
+    return (
+        <>
+{/*            <div> → Timer wrapper*/}
+
+{/*                <span> → Days*/}
+
+{/*<span> → Hours*/}
+
+{/*<span> → Minutes*/}
+
+{/*<span> → Seconds*/}
+
+{/*(Each wrapped in <div> for styling, e.g., "20 Days")*/}
+        </>
+    );
+}
