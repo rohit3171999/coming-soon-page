210e1ea7a6498587dae251587e4cde2e893f54cc
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 15:19:02 +0530

    add functional component in HeroSection.jsx

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,19 @@
+/*
+* Description: The top section of the page that captures attention and communicates the key message.
+Purpose: Immediately tell visitors something exciting is coming, and show the countdown prominently.
+* */
+export default function HeroSection(){
+    return (
+        <>
+            {/*<section> → wrapper*/}
+
+            {/*    <img> or <div> → Logo placeholder*/}
+
+            {/*        <h1> → Headline (“Something Exciting is Coming Soon!”)*/}
+
+            {/*            <p> → Subheadline (short description of app/website)*/}
+
+            {/*                <div> → Wrapper for CountdownTimer component*/}
+        </>
+    );
+}
