008925646b43240cde179f50ab1e5cd1a2b3a493
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 15:36:39 +0530

    add functional component in SocialLinks.jsx

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,19 @@
+/*
+* Description: A section with links to brand’s social media pages.
+Purpose: Allow users to follow the product’s journey before launch.
+* */
+export default function SocialLinks(){
+    return (
+        <>
+            {/*<section> → Wrapper*/}
+
+            {/*    <ul> → List of social icons*/}
+
+            {/*        <li> → Each icon item*/}
+
+            {/*            <a> → Anchor linking to social profile*/}
+
+            {/*                <svg> or <img> → Icon graphic*/}
+        </>
+    );
+}
